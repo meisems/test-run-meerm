72f5097a7c7efb4eea942f15853717c8507e63ae
Emmanuel Joshua Badong <136762621+meisems@users.noreply.github.com>
2026-05-24 10:06:53 +0800

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import AdminApp from './MetroEventsAdmin_v23.jsx'
+import AdminApp from './MetroEventsAdmin_v24.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
